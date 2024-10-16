e2c8829d7ee9b76ba890e02fd79b14e0b53893f3
Andrei Chirun <djanri@gmail.com>
2024-10-16 17:00:00 +0300

    FRONT: added possibility to get user data from telegram api

--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -5,36 +5,44 @@ import { UserContext } from "../components/UserContext"
 import { useEffect, useState } from "react"
 import UserProps from "../models/UserProps"
 import { UsersApiUrl } from "../api_links"
+import { initData, useSignal } from "@telegram-apps/sdk-solid"
 
 const Layout = () => {
     const [userData, setUserData] = useState<UserProps>();
-
+    const initTelegramData = useSignal(initData.state);
     let isIgnore = false;
+
     useEffect(() => {
-        const fetchUserData = async () => {
-            try {
-                if (!isIgnore) {
-                    isIgnore = true;
-                    const response = await fetch(`${UsersApiUrl}/1`);
-                    const data = await response.json();
-                    setUserData(data);
-                    console.log(data);
-                }
-            } catch (error) {
-                console.error("fetch users error,", error);
-            }
-        }
         fetchUserData();
     }, []);
 
     
+    const fetchUserData = async () => {
+        try {
+            if (!isIgnore) {
+                isIgnore = true;
+                const user = initTelegramData()?.user;
+                const response = await fetch(`${UsersApiUrl}/userid/${user?.id}`);
+                const data = await response.json();
+                setUserData(data);
+                console.log(data);
+            }
+        } catch (error) {
+            console.error("fetch users error,", error);
+        }
+    }
+    
 
     return (
+        userData == null
+            ?
+            <div>Not found user</div>
+            :
         <UserContext.Provider value={userData}>
             <Header/>
             <Outlet/>
             <Footer />
-        </UserContext.Provider>
+        </UserContext.Provider >
     )
   }
   
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import './mockEnv.ts';
+import { init } from './init.ts';
+import { retrieveLaunchParams } from '@telegram-apps/sdk-solid';
+
+// Configure all application dependencies.
+init(retrieveLaunchParams().startParam === 'debug');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
